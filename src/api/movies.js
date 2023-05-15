22e234c3e823ac3c52f34ea2e4cbe70b0992d61d
heloisabett <heloisabettramosifc@gmail.com>
2023-05-15 15:01:29 -0300

    parte 1

--- src/api/movies.js
+++ src/api/movies.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class MoviesApi {
   async getMovies() {
     try {
-      const { data } = await axios.get('http://127.0.0.1:19003/movies');
+      const { data } = await axios.get('http://191.52.55.177:19003/movies');
       return Promise.resolve(data);
     } catch (error) {
       return Promise.error(error);
